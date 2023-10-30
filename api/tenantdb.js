bf71795f1c6fef0e112919a737275acf27887439
Arcedo <marcarcedoalvaro25@gmail.com>
2023-10-30 17:35:23 +0100

    activate account and create db to the user

--- api/tenantdb.js
+++ api/tenantdb.js
@@ -1,10 +1,29 @@
 const mysql = require('mysql2');
 
-let connectionMap;
+let connectionMap = new Map();
 
-export async function connectAll (dbs) {
-    connectionMap = new Map();
-    for (const db of dbs) {
+module.exports = {
+    connectAll: async function (dbs) {
+        connectionMap = new Map();
+        for (const db of dbs) {
+            const pool = mysql.createPool({
+                host: db.host,
+                port: db.port,
+                user: db.user,
+                password: db.password,
+                database: db.database,
+                connectionLimit: db.connectionLimit
+            });
+            const promisePool = pool.promise();
+            connectionMap.set(db.id, { pool, promisePool });
+        }
+    },
+
+    getPromisePool: function (dbId) {
+        return connectionMap.get(dbId).promisePool;
+    },
+
+    addConnection: function (db) {
         const pool = mysql.createPool({
             host: db.host,
             port: db.port,
@@ -15,26 +34,9 @@ export async function connectAll (dbs) {
         });
         const promisePool = pool.promise();
         connectionMap.set(db.id, { pool, promisePool });
+    },
+
+    removeConnection: function (dbId) {
+        connectionMap.delete(dbId);
     }
-};
-
-export function getPromisePool (dbId) {
-    return connectionMap.get(dbId).promisePool;
-};
-
-export function addConnection (db) {
-    const pool = mysql.createPool({
-        host: db.host,
-        port: db.port,
-        user: db.user,
-        password: db.password,
-        database: db.database,
-        connectionLimit: db.connectionLimit
-    });
-    const promisePool = pool.promise();
-    connectionMap.set(db.id, { pool, promisePool });
-};
-
-export function removeConnection (dbId) {
-    connectionMap.delete(dbId);
 };
